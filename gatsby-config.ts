1144709e5df17758d5abe3336b7aa560316ecc8d
João César <joao.as.cesar@gmail.com>
2023-09-29 22:51:37 +0100

    fix config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My Personal Website`,
-    siteUrl: `joaoces.netlify.app`
+    siteUrl: `https://joaoces.netlify.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
